53cf4a58e5774f947d7a4fc01ac4491c20f70ff9
Selinace <selinaaragon@MBP-de-Selina.default>
2015-03-04 15:50:24 +0000

    initial commit

---
+++ js/other.js
@@ -0,0 +1,11 @@
+for (var n=15; n <= 300; n++) {
+if (n%3 === 0 && n%5 === 0) {
+  console.log('FizzBuzz');
+} else if (n%3 === 0) {
+  console.log('Fizz');
+} else if (n%5 === 0) {
+  console.log('Buzz');
+} else {
+  console.log(n);
+}
+}
